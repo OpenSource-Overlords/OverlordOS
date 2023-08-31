8ec26d3a5b7bee5a7947661f1249d535bbd00965
Maximillian Mahlke <themaxta@gmail.com>
2023-08-31 13:13:43 -0600

    added example code files

---
+++ code/test.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def test_hello_world():
+    result = subprocess.run(['python', 'hello.py'], capture_output=True, text=True)
+    output = result.stdout.strip()  # This gets the output and removes any leading/trailing whitespaces.
+    
+    assert output == "Hello, World!", f"Expected 'Hello, World!' but got '{output}'"
+
+if __name__ == "__main__":
+    test_hello_world()
+    print("All tests passed!")
