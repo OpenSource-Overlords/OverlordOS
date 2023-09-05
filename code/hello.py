dee76c47f4fba631b91d2484acd972c38c1d64c8
Maximillian Mahlke <themaxta@gmail.com>
2023-09-05 12:08:38 -0600

    test

--- code/hello.py
+++ code/hello.py
@@ -1,2 +1,14 @@
 print("Hello, World!")
 print("Mike Rotch")
+
+print("This message is on max's machine.")
+
+class HelloWorld:
+    def greet(self):
+        return "Hello, World!"
+
+# Create an instance of the HelloWorld class
+hello = HelloWorld()
+
+# Call the greet method and print the result
+print(hello.greet())
